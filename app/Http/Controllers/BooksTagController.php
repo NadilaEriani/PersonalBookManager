b1853af1469ec5b2b7f9347c1f24aad4468a447e
NadilaEriani <nadila23ti@mahasiswa.pcr.ac.id>
2024-12-04 10:56:48 +0700

    Menambahkan model, controller, migration, factory, dan seeder untuk semua table

---
+++ app/Http/Controllers/BooksTagController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\booksTag;
+use App\Http\Requests\StorebooksTagRequest;
+use App\Http\Requests\UpdatebooksTagRequest;
+
+class BooksTagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorebooksTagRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(booksTag $booksTag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(booksTag $booksTag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatebooksTagRequest $request, booksTag $booksTag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(booksTag $booksTag)
+    {
+        //
+    }
+}
---
+++ app/Models/booksTag.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class booksTag extends Model
+{
+    /** @use HasFactory<\Database\Factories\BooksTagFactory> */
+    use HasFactory;
+}
