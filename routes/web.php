3f04a91f45b183e09358646e6454dcddf6073dcf
NadilaEriani <nadila23ti@mahasiswa.pcr.ac.id>
2024-12-11 11:47:19 +0700

    contentnya ke tengah

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Auth\Middleware\Authenticate;
 
-Route::get('/', [HomeController::class, 'index']);
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/', [HomeController::class, 'index']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
