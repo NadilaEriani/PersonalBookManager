bf389296d2a0ad71cb3a8fdd880e95592b79a175
Nadila Eriani <nadila23ti@mahasiswa.pcr.ac.id>
2025-01-08 07:22:26 +0700

    Perubahan terakhir

--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -12,8 +12,8 @@
                 <!-- Judul Buku -->
                 <div class="form-group mt-3">
                     <label for="title">Judul Buku</label>
-                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
-                        name="title" value="{{ old('title') ?? $book->title }}">
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+                        value="{{ old('title') ?? $book->title }}">
                     <span class="text-danger">{{ $errors->first('title') }}</span>
                 </div>
 
@@ -28,16 +28,9 @@
                 <!-- Review -->
                 <div class="form-group mt-3">
                     <label for="review">Review</label>
-                    <textarea class="form-control @error('review') is-invalid @enderror" id="review" name="review">{{ old('review') ?? $book->review }}</textarea>
+                    <textarea class="form-control @error('review') is-invalid @enderror" id="review"
+                        name="review">{{ old('review') ?? $book->review }}</textarea>
                     <span class="text-danger">{{ $errors->first('review') }}</span>
-                </div>
-
-                <!-- Rating -->
-                <div class="form-group mt-3">
-                    <label for="rating">Rating</label>
-                    <input type="number" class="form-control @error('rating') is-invalid @enderror" id="rating"
-                        name="rating" value="{{ old('rating') ?? $book->rating }}" min="1" max="5">
-                    <span class="text-danger">{{ $errors->first('rating') }}</span>
                 </div>
 
                 <!-- Tombol Submit -->
